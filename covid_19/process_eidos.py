08374edca96c8738ba54c0fe161ce61d87b8c434
Ben Gyori <ben.gyori@gmail.com>
2020-03-17 15:23:03 -0400

    Add script for processing abstracts with Eidos

---
+++ covid_19/process_eidos.py
@@ -0,0 +1,26 @@
+"""This script searches for relevant PMIDs on PubMed, and then
+reads the abstracts corresponding to each PMID with Eidos."""
+import time
+import pickle
+from tqdm import tqdm
+from indra.sources import eidos
+from indra.literature import pubmed_client
+
+
+keywords = ['covid19', 'covid-19', 'sars-cov-2', 'sars-cov2']
+ids = []
+for kw in keywords:
+    ids += pubmed_client.get_ids(kw)
+
+
+stmts = {}
+for pmid in tqdm(ids):
+    time.sleep(3)
+    abst = pubmed_client.get_abstract(pmid)
+    if not abst:
+        continue
+    ep = eidos.process_text(abst, webservice='http://localhost:9000/')
+    stmts[pmid] = ep.statements
+
+with open('../data/eidos_abstract_stmts.pkl', 'wb') as fh:
+    pickle.dump(stmts, fh)
