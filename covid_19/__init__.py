363b7e8bc2c0669ad49e7ca9d69d71225a18d8f1
Ben Gyori <ben.gyori@gmail.com>
2020-03-26 16:02:13 -0400

    Add module functions to process metadata and get text refs

---
+++ covid_19/__init__.py
@@ -0,0 +1,32 @@
+import re
+import csv
+
+
+def read_metadata(fname):
+    entries = []
+    with open(fname, 'r') as fh:
+        reader = csv.reader(fh)
+        header = next(reader)
+        header[0] = 'ID'
+        for row in reader:
+            entry = {h: str(v) for h, v in zip(header, row)}
+            entries.append(entry)
+    return entries
+
+
+def get_text_refs_from_metadata(entry, metadata_version='1'):
+    mappings = {
+        'ID': 'CORD19_INDRA_%s' % metadata_version,
+        'sha': 'CORD19_SHA',
+        'doi': 'DOI',
+        'pmcid': 'PMCID',
+        'pubmed_id': 'PMID',
+        'WHO #Covidence': 'WHO_COVIDENCE',
+        'Microsoft Academic Paper ID': 'MICROSOFT'
+    }
+    text_refs = {}
+    for key, ref_key in mappings.items():
+        val = entry.get(key)
+        if val:
+            text_refs[ref_key] = val
+    return text_refs
