38f1c7096ad66d0cfb819ed4b285384fb9f5f57f
John Bachman <bachmanjohn@gmail.com>
2020-03-28 14:27:52 -0400

    Add path to gordon stmts file

--- covid_19/process_gordon_ndex.py
+++ covid_19/process_gordon_ndex.py
@@ -1,8 +1,11 @@
 import pickle
 from indra.sources import ndex_cx
 
+stmts_file = join(dirname(abspath(__file__)), '..', 'stmts',
+                          'gordon_ndex_stmts.pkl')
+
 cxp = ndex_cx.process_ndex_network('43803262-6d69-11ea-bfdc-0ac135e8bacf',
                                    require_grounding=False)
 
-with open('../gordon_ndex_stmts.pkl', 'wb') as fh:
+with open(stmts_file, 'wb') as fh:
     pickle.dump(cxp.statements, fh)
