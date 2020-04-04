d79472b78460be6ef4c2c082c6ecc25312e0f869
Ben Gyori <ben.gyori@gmail.com>
2020-04-04 17:39:17 -0400

    Fix invalid DOIs

--- covid_19/__init__.py
+++ covid_19/__init__.py
@@ -13,6 +13,16 @@ def read_metadata(fname):
     return entries
 
 
+def fix_doi(doi):
+    if doi is None:
+        return None
+    prefixes = ['http://dx.doi.org/', 'doi.org/']
+    for prefix in prefixes:
+        if doi.startswith(prefix):
+            doi = doi[len(prefix):]
+    return doi
+
+
 def get_text_refs_from_metadata(entry, metadata_version='1'):
     mappings = {
         'ID': 'CORD19_INDRA_V%s' % metadata_version,
@@ -27,6 +37,8 @@ def get_text_refs_from_metadata(entry, metadata_version='1'):
     for key, ref_key in mappings.items():
         val = entry.get(key)
         if val:
+            if key == 'doi':
+                val = fix_doi(val)
             # Temporary patch to remove float suffixes
             if val.endswith('.0'):
                 val = val[:-2]
