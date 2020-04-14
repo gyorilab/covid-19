7588885806e95ff1611ceea76f294149337f2a29
Ben Gyori <ben.gyori@gmail.com>
2020-04-14 10:53:05 -0400

    Add grounding table for raw statements and Gilda grounding

--- covid_19/grounding_curation.py
+++ covid_19/grounding_curation.py
@@ -28,7 +28,7 @@ def get_eidos_gilda_grounding_counts(stmts):
             ev_text_for_agent_text[txt] = (stmt.evidence[0].pmid,
                                            stmt.evidence[0].text)
             texts.append((txt, ('%s:%s' % gr) if gr[0] else '',
-                          standard_name, url))
+                          standard_name, url, ''))
     # Count the unique text-grounding entries
     cnt = Counter(texts)
     return cnt, ev_text_for_agent_text
@@ -68,10 +68,38 @@ def get_text_grounding_counts(stmts):
                 # These entries are presumably overwritten many times but
                 # that's okay.
                 ev_text_for_agent_text[t] = (ev.pmid, ev.text)
+                gilda_grounding = gilda.ground(t)
+                gilda_grounding = '%s:%s' % (gilda_grounding[0].term.db,
+                                             gilda_grounding[0].term.id) \
+                    if gilda_grounding else ''
                 # We now add a new entry to the text-grounding list
                 texts.append((t, ('%s:%s' % gr) if gr[0] else '',
-                              standard_name, url))
+                              standard_name, url, gilda_grounding))
     # Count the unique text-grounding entries
+    cnt = Counter(texts)
+    return cnt, ev_text_for_agent_text
+
+
+def get_raw_statement_text_grounding_counts(stmts):
+    texts = []
+    ev_text_for_agent_text = {}
+    for stmt in stmts:
+        for agent in stmt.agent_list():
+            if agent is None:
+                continue
+            txt = agent.db_refs['TEXT']
+            ev_text_for_agent_text[txt] = (stmt.evidence[0].pmid,
+                                           stmt.evidence[0].text)
+            assert txt, agent.db_refs
+            gr = agent.get_grounding()
+            standard_name = name_from_grounding(*gr) if gr[0] else ''
+            url = get_identifiers_url(*gr) if gr[0] is not None else ''
+            gilda_grounding = gilda.ground(txt)
+            gilda_grounding = '%s:%s' % (gilda_grounding[0].term.db,
+                                         gilda_grounding[0].term.id) \
+                if gilda_grounding else ''
+            texts.append((txt, ('%s:%s' % gr) if gr[0] else '',
+                          standard_name, url, gilda_grounding))
     cnt = Counter(texts)
     return cnt, ev_text_for_agent_text
 
@@ -85,8 +113,8 @@ def load_stmts(fname):
 
 def dump_table(text_grounding_cnt, ev_text_for_agent_text, fname):
     # Dump the results into a TSV file
-    rows = [['text', 'grounding', 'standard_name', 'url', 'count',
-             'pmid', 'ev_text']]
+    rows = [['text', 'grounding', 'standard_name', 'url', 'gilda_grounding',
+             'count', 'pmid', 'ev_text']]
     for data, count in text_grounding_cnt.most_common():
         pmid, ev_text = ev_text_for_agent_text[data[0]]
         row = list(data) + [str(count), pmid, ev_text]
@@ -103,3 +131,4 @@ if __name__ == '__main__':
     cnt, ev_text_for_agent_text = get_eidos_gilda_grounding_counts(stmts)
     dump_table(cnt, ev_text_for_agent_text,
                '../grounding_eidos_gilda_table.tsv')
+
