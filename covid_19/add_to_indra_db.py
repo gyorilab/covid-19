8a97094f645939a36edfd7aef4184b1e7074640b
John Bachman <bachmanjohn@gmail.com>
2020-04-18 12:24:43 -0400

    Delete old/commented code

--- covid_19/add_to_indra_db.py
+++ covid_19/add_to_indra_db.py
@@ -15,18 +15,7 @@ logger = logging.getLogger(__name__)
 content_manager.logger.setLevel(logging.DEBUG)
 
 
-"""
-def get_val(row, col):
-    df_cols = [
-      'index', 'sha', 'source_x', 'title', 'doi', 'pmcid', 'pubmed_id',
-      'license', 'abstract', 'publish_time', 'authors', 'journal',
-      'Microsoft Academic Paper ID', 'WHO #Covidence', 'has_full_text',
-      'full_text_file', 'content_path', 'content_type']
-    val = row[df_cols.index(col)]
-    return None if pd.isna(val) else val
-"""
-
-class Cord19Manager(PmcManager):
+class Cord19Manager(ContentManager):
     my_source = 'cord19'
     #tr_cols = ('pmid', 'pmcid', 'doi', 'cord19_uid')
     tr_cols = ('pmid', 'pmcid', 'doi')
@@ -48,24 +37,8 @@ class Cord19Manager(PmcManager):
                              'doi': doi}
             self.tr_data.append(tr_data_entry)
 
-        """
-        self.cord_trs = get_unique_text_refs()
-        # Dict keyed by text ref ID 
-        self.tr_dicts = cord19_metadata_for_trs(self.cord_trs, cord_md)
-        ref_key_map = {'PMID': 'pmid'}
-        for tr_id, tr_refs in self.tr_dicts.items():
-            tr_data_dict = {'pmid': None, 'pmcid': None, 'doi': None,
-                            'manuscript_id': None}
-            for key, val in tr_refs.items():
-                if key not in ref_key_map:
-                    continue
-                mapped_key = ref_key_map[key]
-                tr_data_dict[mapped_key] = val
-            self.tr_data.append(tr_data_dict)
-        """
 
     def populate(self, db):
-
         # Turn the list of dicts into a set of tuples
         tr_data_set = {tuple([entry[id_type] for id_type in self.tr_cols])
                        for entry in self.tr_data}
@@ -120,77 +93,6 @@ class Cord19Manager(PmcManager):
         #    )
         gatherer.add('content', len(filtered_tc_records))
         return
-
-
-    """
-    def upload_batch(self):
-        # Turn the list of dicts into a set of tuples
-        tr_data_set = {tuple([entry[id_type] for id_type in self.tr_cols])
-                       for entry in tr_data}
-
-        filtered_tr_records, flawed_tr_records = \
-            self.filter_text_refs(db, tr_data_set,
-                                  primary_id_types=['pmid', 'pmcid',
-                                                    'manuscript_id'])
-
-       a 
-
-
-    def upload_batch(self, db, tr_data, tc_data, get_missing_pmids=True):
-        # Modifies the tr_data dictionary in place
-        if get_missing_pmids:
-            tr_data = self.get_missing_pmids(db, tr_data)
-
-
-    def get_missing_pmids(self, db, tr_data):
-        tr_data_pmid = []
-        tr_data_pmcid = []
-        tr_data_doi = []
-        tr_data_none = []
-        for tr in tr_data:
-            if tr['pmid'] is not None:
-                tr_data_pmid.append(tr)
-            else:
-                if tr['pmcid'] is not None:
-                    tr_data_pmcid.append(tr)
-                else:
-                    if tr['doi'] is not None:
-                        tr_data_doi.append(tr)
-                    else:
-                        tr_data_none.append(tr)
-        print(len(tr_data_pmcid), 'pmcid but no pmid')
-        # Fill in PMIDs for the papers for which we have a PMCID
-        super(Cord19Manager, self).get_missing_pmids(db, tr_data_pmcid)
-        # Fill in PMIDs for the papers for which we have a DOI (or nothing)
-        tr_list = db.select_all(
-            db.TextRef, db.TextRef.doi.in_(
-                [tr_entry['doi'] for tr_entry in tr_data_doi]
-                )
-            )
-        pmids_from_db = {tr.doi: tr.pmid for tr in tr_list
-                         if tr.pmid is not None}
-
-        logger.debug("Found %d pmids on the databse." % len(pmids_from_db))
-        num_found_non_db = 0
-        for tr_entry in tr_data_doi:
-            if tr_entry['doi'] not in pmids_from_db.keys():
-                pass
-                #ret = id_lookup(tr_entry['pmcid'], idtype='pmcid')
-                #if 'pmid' in ret.keys() and ret['pmid'] is not None:
-                #    tr_entry['pmid'] = ret['pmid']
-                #    num_found_non_db += 1
-                #    num_missing -= 1
-            else:
-                tr_entry['pmid'] = pmids_from_db[tr_entry['doi']]
-                #num_missing -= 1
-        tr_combined = tr_data_pmid + tr_data_pmcid + tr_data_doi + tr_data_none
-        tr_no_pmid = []
-        for tr in tr_combined:
-            if tr['pmid'] is None:
-                tr_no_pmid.append(tr)
-        print(len(tr_no_pmid), 'no pmid')
-        return tr_combined
-    """
 
 
 if __name__ == '__main__':
