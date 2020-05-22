9a0ed3972712eb9713f7461c5fb7fcbfc80abae8
Ben Gyori <ben.gyori@gmail.com>
2020-05-22 00:39:22 -0400

    Add disease map ID mappings script

---
+++ covid_19/disease_maps/id_mapping.py
@@ -0,0 +1,99 @@
+import json
+import pandas
+import requests
+from collections import defaultdict
+from indra.statements import stmts_from_json
+from indra.preassembler.grounding_mapper.standardize import \
+    standardize_db_refs
+from indra.databases import get_identifiers_url, hgnc_client
+from emmaa.util import find_latest_s3_file, get_s3_client
+
+
+def get_bigg_chebi_mappings():
+    df = pandas.read_csv(
+        'http://bigg.ucsd.edu/static/namespace/bigg_models_metabolites.txt',
+        sep='\t')
+    mappings = defaultdict(list)
+    for _, row in df.iterrows():
+        if pandas.isna(row['database_links']):
+            continue
+        links = row['database_links'].split('; ')
+        for link in links:
+            if link.startswith('CHEBI'):
+                chebi_id = link[6:].rsplit('/')[-1]
+                mappings[row['bigg_id']].append(chebi_id)
+    return dict(mappings)
+
+
+if __name__ == '__main__':
+    # Get INDRA statements from latest EMMAA model
+    s3 = get_s3_client(unsigned=True)
+    latest_model_json = find_latest_s3_file('emmaa', 'assembled/covid19')
+    stmts_obj = s3.get_object(Bucket='emmaa', Key=latest_model_json)
+    stmts = stmts_from_json(json.loads(stmts_obj['Body'].read().decode('utf-8')))
+    indra_groundings = set()
+    for stmt in stmts:
+        for agent in stmt.agent_list():
+            if agent is None:
+                continue
+            for k, v in agent.db_refs.items():
+                if k not in {'TEXT', 'TEXT_NORM'}:
+                    indra_groundings.add((k, v))
+
+    # Read disease maps URLs
+    url = ('https://raw.githubusercontent.com/cannin/covid19-analysis/'
+           'master/disease_maps_ids.txt')
+    dm_urls = requests.get(url).text.split('\n')[:-1]
+
+    bigg_to_chebi = get_bigg_chebi_mappings()
+
+    matches = []
+    identifiers_prefix = 'https://identifiers.org/'
+    for dm_url in dm_urls:
+        # We do it this way instead of splitting because of DOIs which have
+        # extra slashes
+        entity = dm_url[len(identifiers_prefix):]
+        db_ns, db_id = entity.split(':', maxsplit=1)
+        if db_ns == 'CHEBI':
+            db_refs = [standardize_db_refs({'CHEBI': '%s:%s' % (db_ns, db_id)})]
+        elif db_ns == 'hgnc':
+            db_refs = [standardize_db_refs({'HGNC': db_id})]
+        elif db_ns == 'hgnc.symbol':
+            hgnc_id = hgnc_client.get_current_hgnc_id(db_id)
+            db_refs = [standardize_db_refs({'HGNC': hgnc_id})]
+        elif db_ns == 'pubchem.compound':
+            db_refs = [standardize_db_refs({'PUBCHEM': db_id})]
+        elif db_ns == 'uniprot':
+            db_refs = [standardize_db_refs({'UP': db_id})]
+        elif db_ns == 'bigg.metabolite':
+            chebi_ids = bigg_to_chebi.get(db_id)
+            if chebi_ids:
+                db_refs = [standardize_db_refs({'CHEBI': chebi_id})
+                           for chebi_id in chebi_ids]
+            else:
+                db_refs = [{}]
+        elif db_ns == 'ncbigene':
+            hgnc_id = hgnc_client.get_hgnc_from_entrez(db_id)
+            if hgnc_id:
+                db_refs = [standardize_db_refs({'HGNC': hgnc_id})]
+            else:
+                db_refs = [{}]
+        # Skip literature references that aren't entities
+        elif db_ns in {'doi', 'pubmed'}:
+            continue
+        else:
+            print('Unhandled namespace %s' % db_ns)
+            db_refs = {}
+
+        matched = None
+        for db_ref in db_refs:
+            for k, v in db_ref.items():
+                if (k, v) in indra_groundings:
+                    matched = (k, v)
+                    break
+
+        matches.append((dm_url,
+                        get_identifiers_url(*matched) if matched else None))
+    with open('diseasemap_indra_mappings.csv', 'w') as fh:
+        for m1, m2 in matches:
+            fh.write('%s,%s\n' % (m1, m2 if m2 else ''))
